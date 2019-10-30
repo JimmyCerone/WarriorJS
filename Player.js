4aff0e53e6edc7af14a6235263d2ed1f6c0144d1
JimmyCerone <james.cerone@hope.edu>
2019-10-30 13:10:04 -0400

    Basic Warrior

---
+++ Player.js
@@ -0,0 +1,60 @@
+class Player {
+  // health check
+  constructor() {
+      this.health = 20;
+  }
+
+
+// TODO FIX THE HEALTH CHECK TO SEE IF THERE IS AN ARCHER
+
+  playTurn(warrior) {
+      // if you're busted up, be careful
+      if (this.isBustedUp(warrior)) {
+          // if your current health is less than the last turn
+          if(this.health > warrior.health()) {
+            // if the warrior is in front of you, attack
+            if(this.isEnemy(warrior)) {
+              warrior.attack();
+            }
+            // if the warrior isn't right in front of you, walk towards them
+            else if(!this.isEnemy(warrior)) {
+              warrior.walk();
+            }
+          }
+          else {
+            // if you aren't losing health, walk back
+            if(this.isEnemy(warrior)) {
+              warrior.walk('backward');
+            }
+            // if you are away, rest
+            else {
+              warrior.rest();
+            }
+          }
+      }
+      // if you're all good, walk forward or attack
+      else {
+        // if there's an enemy, attack
+        if(this.isEnemy(warrior)) {
+            warrior.attack();
+        }
+        // otherwise go find one
+        else {
+            warrior.walk();
+        }
+      }
+      this.health = warrior.health();
+  }
+
+  // checks to see if your health is higher than 15
+  isBustedUp(warrior) {
+    return warrior.health() < 20;
+  }
+
+  // checks to see if there is an enemy in front of you
+  // returns true if there is
+  isEnemy(warrior) {
+      return !(warrior.feel().isEmpty());
+  }
+
+}
