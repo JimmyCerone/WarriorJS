e663293a9832cbdbef3bfb5ec020b8f4af55323e
JimmyCerone <james.cerone@hope.edu>
2019-11-01 18:33:59 -0400

    first attempt at FSM -- BAD --

--- Player.js
+++ Player.js
@@ -2,60 +2,45 @@ class Player {
   // health check
   constructor() {
       this.health = 20;
+      this.state = '';
   }
 
   playTurn(warrior) {
-      // if you're busted up, be careful
-      if (this.isBustedUp(warrior)) {
-          // if your current health is less than the last turn
-          if(this.health > warrior.health()) {
-            // if the warrior is in front of you, attack
-            if(this.isEnemy(warrior)) {
-              warrior.attack();
-            }
-            // if the warrior isn't right in front of you, walk towards them
-            else if(!this.isEnemy(warrior)) {
-              warrior.walk();
-            }
-          }
-          // if there's no enemy and you are losing health, heal up
-          else if(!this.isEnemy(warrior)) {
-            warrior.rest();
+      this.state = this.stateFinder(warrior);
+      switch(this.state) {
+        case 'hurt':
+          warrior.rest();
+          break;
+        case 'healthy':
+          if(this.isEnemy(warrior)) {
+            warrior.attack();
           }
           else {
-            // if you aren't losing health, walk back
-            if(this.isEnemy(warrior)) {
-              warrior.walk('backward');
-            }
-            // if you are away, rest
-            else {
-              warrior.rest();
-            }
-          }
-      }
-      // if you're all good, walk forward or attack
-      else {
-        // if there's an enemy, attack
-        if(this.isEnemy(warrior)) {
-            warrior.attack();
-        }
-        // otherwise go find one
-        else {
             warrior.walk();
-        }
+          }
+          break;
+        default:
+          warrior.think("I am losing");
       }
       this.health = warrior.health();
   }
 
-  // checks to see if your health is higher than 15
-  isBustedUp(warrior) {
-    return warrior.health() < 20;
+  stateFinder(warrior) {
+      if(this.health == 20) {
+          return 'healthy';
+      }
+      else {
+         return 'hurt';
+      }
   }
 
-  // checks to see if there is an enemy in front of you
-  // returns true if there is
   isEnemy(warrior) {
-      return !(warrior.feel().isEmpty());
+    if(warrior.feel().isUnit()) {
+        return warrior.feel().getUnit().isEnemy();
+    }
+    else {
+      return false;
+    }
   }
 
 }
